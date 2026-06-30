637fa6414eb674841da92e9f9ba1ca6f8818f330
VERONESS <turokromaha@mail.ru>
2026-07-01 02:59:42 +0300

    Add files via upload

---
+++ 1.07.26.py
@@ -0,0 +1,20 @@
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+# Additionally, if the number is negative, return 0.
+# Note: If a number is a multiple of both 3 and 5, only count it once.
+#
+# def solution(l):
+#     a = [i for i in l if i != 0]
+#     if l < 0:
+#         return 0
+#     else:
+#         return sum([i for i in l if i%3 == 0 or i%5 == 0])
+#
+# solution([3,5,15,0])#result is 23
+
+
+def solution(l):
+    if l < 0:
+        return 0
+    else:
+        return sum([i for i in range(l) if i % 3 == 0 or i % 5 == 0])
