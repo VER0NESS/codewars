ff7e81a289dd0225f3bb54a3849ffdd7ea9bc57f
VERONESS <turokromaha@mail.ru>
2026-09-04 06:36:40 +0300

    Add files via upload

---
+++ 26.09.04.py
@@ -0,0 +1,18 @@
+def multi_table(number):
+        print(f"1 * {number} = {number*1}\n2 * {number} = {number*2}\n3 * {number} = {number*3}\n4 * {number} = {number*4}\n5 * {number} = {number*5}\n6 * {number} = {number*6}\n7 * {number} = {number*7}\n8 * {number} = {number*8}\n9 * {number} = {number*9}\n10 * {number} = {number*10}\n")
+        
+multi_table(5)
+
+
+# 1 * 5 = 5
+# 2 * 5 = 10
+# 3 * 5 = 15
+# 4 * 5 = 20
+# 5 * 5 = 25
+# 6 * 5 = 30
+# 7 * 5 = 35
+# 8 * 5 = 40
+# 9 * 5 = 45
+# 10 * 5 = 50
+
+# number = 5
