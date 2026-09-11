9bb8faee2cac90a261d7af0eac79c8ab90ec7f5b
VERONESS <turokromaha@mail.ru>
2026-09-11 13:24:48 +0300

    Add files via upload

---
+++ 26.09.11(3).py
@@ -0,0 +1,7 @@
+def high_and_low(numbers):
+    a=(max(numbers.split(" ")))
+    b=(min(numbers.split(" ")))
+    print(f"{a} {b}")
+high_and_low("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6")
+
+# my output: '6 -214
